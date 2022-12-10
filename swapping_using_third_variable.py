4cfe154ab920f268de93759b76410e51f26b3608
kunalquantum <87424424+kunalquantum@users.noreply.github.com>
2022-12-10 14:58:00 +0530

    swapping of the values of two variables
    we have used the third variable to exchange the values of the variables

---
+++ swapping_using_third_variable.py
@@ -0,0 +1,24 @@
+
+
+#accepting the values from the user (run time)
+a=int(input("enter the value for the first variable :"))
+b=int(input("enter the value for the second variable :"))
+print("before swapping:")
+print("value of a :",a)
+print("value of b",b)
+#(process of swapping the values)
+
+temp=0
+temp=a
+a=b
+b=temp
+# priting the outputs
+print("after  swapping:")
+print("value of a :",a)
+print("value of b :",b)
+
+
+
+
+
+
